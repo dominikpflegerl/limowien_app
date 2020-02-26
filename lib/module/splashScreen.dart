7a87ee3659b556b438448394e2ee51f63bd7fb3c
Dominik Pflegerl <github.me@neus.xyz>
2020-02-26 18:21:34 +0100

    - refactored loginView -> welcomeView - redesigned welcomeView - added light statusbaricons to welcomeView & splashscreen - fixed navigation drawer color under statusbar (before dark -> icons barely visible, now goldish)

--- lib/module/splashScreen.dart
+++ lib/module/splashScreen.dart
@@ -1,11 +1,11 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart'; // statusbar color
 
 import 'package:splashscreen/splashscreen.dart';
 
-import 'package:limowien_app/module/loginView.dart';
-//import 'package:limowien_app/main.dart';
+import 'package:limowien_app/module/welcomeView.dart';
 
 class AppLaunch extends StatefulWidget {
   @override
@@ -15,22 +15,26 @@ class AppLaunch extends StatefulWidget {
 class AppSplashScreen extends State<AppLaunch> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarIconBrightness: Brightness.light,
+      statusBarColor: Colors.transparent,
+    ));
     return new SplashScreen(
         seconds: 3,
-        navigateAfterSeconds: new LoginView(),
+        navigateAfterSeconds: new WelcomeView(),
         title: new Text('limowien',
           style: new TextStyle(
               color: Color(0xFFdfddde),
               fontFamily: 'TimesNewRoman',
               //fontWeight: FontWeight.bold,
               fontSize: 50.0,
-              height: 1
+              height: 2,
           ),),
         image: new Image.asset('assets/images/logo.png'),
         backgroundColor: Color(0xFF221f1c),
         styleTextUnderTheLoader: new TextStyle(),
-        photoSize: 100.0,
-        loaderColor: Color(0xFFb69862),
+        photoSize: 80.0,
+        loaderColor: Color(0xFFE2E0E1FF),
     );
   }
 }
--- lib/widgets/drawerWidget.dart
+++ lib/widgets/drawerWidget.dart
@@ -9,7 +9,7 @@ import '../routes/paymentView.dart';
 import '../routes/settingsView.dart';
 //import '../routes/logoutView.dart';
 import '../routes/aboutView.dart';
-import '../module/loginView.dart';
+import '../module/welcomeView.dart';
 
 
 class NavigationDrawer extends StatelessWidget {
@@ -17,12 +17,12 @@ class NavigationDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Drawer(
         child: Container(
-          color: Color(0xFF221f1c),
+          color: Color(0xFFb69862),
           child: new ListView(
             children: <Widget>[
               new UserAccountsDrawerHeader(
-                accountName: new Text("Dominik"),
-                accountEmail: new Text("dominik@limowien.com"),
+                accountName: new Text("John Doe", style: TextStyle(fontSize: 16)),
+                accountEmail: new Text("test@limowien.com"),
                 decoration: new BoxDecoration(
                   color: Color(0xFFb69862),
                   image: new DecorationImage(
@@ -33,50 +33,54 @@ class NavigationDrawer extends StatelessWidget {
                 currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage("https://randomuser.me/api/portraits/men/46.jpg")),
               ),
               // Menu Structure
-              new ListTile(
-                leading: new Icon(MdiIcons.history,color: Colors.white),
-                title: new Text("Deine Fahrten", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new HistoryView(),));},
-              ),
-              new ListTile(
-                leading: new Icon(MdiIcons.history,color: Colors.white),
-                title: new Text("LoginView", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new LoginView(),));},
-              ),
-              new ListTile(
-                leading: new Icon(MdiIcons.carMultiple,color: Colors.white),
-                title: new Text("Flottenübersicht", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new FleetView(),));},
-              ),
-              new Divider(color: Colors.white),
-              new ListTile(
-                leading: new Icon(MdiIcons.account,color: Colors.white),
-                title: new Text("Konto", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new AccountView(),));},
-              ),
-              new ListTile(
-                leading: new Icon(MdiIcons.cash,color: Colors.white),
-                title: new Text("Bezahlung", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new PaymentView(),));},
-              ),
-              new ListTile(
-                leading: new Icon(MdiIcons.settings,color: Colors.white),
-                title: new Text("Einstellungen", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new SettingsView(),));},
-              ),
-              new Divider(color: Colors.white),
-              new ListTile(
-                leading: new Icon(MdiIcons.logout,color: Colors.white),
-                title: new Text("Abmelden", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new LoginView(),));},
-              ),              new Divider(color: Colors.white),
-              new ListTile(
-                leading: new Icon(MdiIcons.information,color: Colors.white),
-                title: new Text("About", style: new TextStyle(color: Colors.white),),
-                onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new AboutView(),));},
-              ),
-              new ListTile(
-                title: new Text("© Limowien GmbH", style: new TextStyle(color: Colors.white, fontSize: 9),),
+              Container(
+                padding: EdgeInsets.only(top: 10),
+                color: Color(0xFF221f1c),
+                child:
+                  Column(
+                    children: <Widget>[
+                      new ListTile(
+                        leading: new Icon(MdiIcons.history,color: Colors.white),
+                        title: new Text("Deine Fahrten", style: new TextStyle(color: Colors.white),),
+                        onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new HistoryView(),));},
+                      ),
+                      new ListTile(
+                        leading: new Icon(MdiIcons.carMultiple,color: Colors.white),
+                        title: new Text("Flottenübersicht", style: new TextStyle(color: Colors.white),),
+                        onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new FleetView(),));},
+                      ),
+                      new Divider(color: Colors.white),
+                      new ListTile(
+                        leading: new Icon(MdiIcons.account,color: Colors.white),
+                        title: new Text("Konto", style: new TextStyle(color: Colors.white),),
+                        onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new AccountView(),));},
+                      ),
+                      new ListTile(
+                        leading: new Icon(MdiIcons.cash,color: Colors.white),
+                        title: new Text("Bezahlung", style: new TextStyle(color: Colors.white),),
+                        onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new PaymentView(),));},
+                      ),
+                      new ListTile(
+                        leading: new Icon(MdiIcons.settings,color: Colors.white),
+                        title: new Text("Einstellungen", style: new TextStyle(color: Colors.white),),
+                        onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new SettingsView(),));},
+                      ),
+                      new Divider(color: Colors.white),
+                      new ListTile(
+                        leading: new Icon(MdiIcons.logout,color: Colors.white),
+                        title: new Text("Abmelden", style: new TextStyle(color: Colors.white),),
+                        onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new WelcomeView(),));},
+                      ),              new Divider(color: Colors.white),
+                      new ListTile(
+                        leading: new Icon(MdiIcons.information,color: Colors.white),
+                        title: new Text("About", style: new TextStyle(color: Colors.white),),
+                        onTap: () {Navigator.pop(context); Navigator.push(context, new MaterialPageRoute(builder: (BuildContext context) => new AboutView(),));},
+                      ),
+                      new ListTile(
+                        title: new Text("© Limowien GmbH", style: new TextStyle(color: Colors.white, fontSize: 9),),
+                      ),
+                    ],
+                  ),
               ),
             ],
           ),
